cdffd8612f0af27b111a16cabda86a6270af7327
Mark van der Loo <mark.vanderloo@gmail.com>
2012-07-19 17:02:45 +0200

    .

--- examples/sparseConstraints.R
+++ examples/sparseConstraints.R
@@ -7,16 +7,17 @@ library(editrules)
 E <- editmatrix(expression(
     x1 - x5 + x8 == 0,
     x5 - x3 - x4 == 0,
-    x8 - x6 - x7 == 0,
-    x1 > 0,
-    x3 > 0,
-    x5 > 0,
-    x6 > 0,
-    x7 > 0,
-    x8 > 0
+    x8 - x6 - x7 == 0
+#    x4 > 0
+#    x3 > 0,
+#    x5 > 0,
+#    x6 > 0,
+#    x7 > 0,
+#    x8 > 0
 ))
-x <- c(x1=34,x2=2,x3=100,x4=3,x5=103,x6=50,x7=20,x8=70)*10
+x <- c(x1=33,x2=2,x3=100,x4=3,x5=95,x6=50,x7=20,x8=70)*10
 
+E <- substValue(E,'x5',x['x5'])
 
 
 violatedEdits(E,x,tol=1e-4)
@@ -35,7 +36,7 @@ I
 u <- x[I]
 w <- rep(1,length(u))
 
-y <- adapt(e,u,tol=1e-5)
+y <- adapt(e,u,w=1/u,tol=1e-5)
 y
 xt <-  x;
 xt[I] <- y$sol;
