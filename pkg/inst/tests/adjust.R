efe7e2f13dd4044adcc64bf3f3b0c1aa60c4e6e9
mark <mark.vanderloo@gmail.com>
2012-08-01 09:35:24 +0200

    added test

--- pkg/inst/tests/adjust.R
+++ pkg/inst/tests/adjust.R
@@ -50,5 +50,11 @@ test_that("adjust.matrix",{
    expect_error( adjust(A,b,c(0,0,0),neq=neq) )
 })
 
+test_that("adjust notes exceeding maxiter"){
+   e <- editmatrix(expression(x < -1, x > 0))
+   expect_true(grep("maximum",adjust(e,0)))
+}
+
+
 
 
