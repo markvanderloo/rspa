c93890d5ab0a7340b3884ef4274231acfd8aa424
markvanderloo <mark.vanderloo@gmail.com>
2017-06-12 12:12:21 +0200

    updated docs. ready for CRAN

--- pkg/R/rspa.R
+++ pkg/R/rspa.R
@@ -2,16 +2,17 @@
 #' 
 #' @section Overview:
 #'
-#' {Given a vector \eqn{\boldsymbol{x}^0}, and a set linear restrictions of the
+#' Given a vector \eqn{\boldsymbol{x}^0}, and a set linear restrictions of the
 #' form  \eqn{\boldsymbol{a}_i\cdot\boldsymbol{x}_i=b_i} and/or 
 #' \eqn{\boldsymbol{a}_i\cdot\boldsymbol{x}_i\leq b_i} with \eqn{i=1,2,\ldots,m}. This package finds the 
 #' nearest vector to \eqn{\boldsymbol{x}^0} (in the (weighted) euclidean sense)
-#' that obeys all restrictions. 
+#' that satisfies all restrictions. 
 #' 
-#' The package can handle large (sparse) problemes and has been tested by us on
-#' vectors with half a million variables and sixty-thousand constraints. All underlying
-#' algorithms have been implemented as a \code{C} library.
-#' }
+#' Much of this package's functionality, including algorithms for working
+#' with large, sparse problems has been moved to the \code{lintools} package.
+#' This package will serve as a front-end for application of the succsessive
+#' projection algorithm for data stored in data.frame like objects.
+#' 
 #' @name rspa-package
 #' @docType package
 #' @useDynLib rspa, .registration=TRUE
