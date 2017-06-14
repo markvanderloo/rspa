9c2ab90c213183e045010e99fd1c7dc8a54ea47c
markvanderloo <mark.vanderloo@gmail.com>
2017-06-14 10:06:00 +0200

    added tests

--- pkg/tests/testthat/test_match_restrictions.R
+++ pkg/tests/testthat/test_match_restrictions.R
@@ -19,5 +19,25 @@ test_that("adjusts correctly",{
 })
 
 
+context("tagging")
+
+test_that("tagging works",{
+  w <- women
+  w[1,1] <- NA
+  w <- tag_missing(w)
+  expect_identical(tagged_values(w),is.na(w))
+})
+
+test_that("tags are picked up",{
+  d <- data.frame(x=NA,y=0.5)
+  d <- tag_missing(d)
+  d$x <- 1
+  v <- validator(x + y == 1, x>0,y>0)
+  expect_equivalent(
+    match_restrictions(d,v)
+    , data.frame(x=0.5,y=0.5)
+  )
+})
+
 
 
